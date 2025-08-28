23b682a027f559e36dee31292a9ce8d8787412f7
Sandeep Raina <127074583+Sandeepraina40@users.noreply.github.com>
2025-08-28 11:44:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.querySelector('input[type="button"]').addEventListener("click",function(){
+	let colorSelect = document.getElementById("colorSelect");
+	let selectIndex = colorSelect.selectIndex;
+	if(selectIndex !== -1){
+		colorSelect.remove(selectIndex);
+	}
+});
+
