6232348d88de5e154f99f15f04f5fe73bae56b53
Sandeep Raina <127074583+Sandeepraina40@users.noreply.github.com>
2025-08-28 11:50:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-//your JS code here. If required.
-document.querySelector('input[type="button"]').addEventListener("click",function(){
-	let colorSelect = document.getElementById("colorSelect");
-	let selectIndex = colorSelect.selectIndex;
-	if(selectIndex !== -1){
-		colorSelect.remove(selectIndex);
-	}
+document.querySelector('input[type="button"]').addEventListener("click", function() {
+    let colorSelect = document.getElementById("colorSelect");
+    for (let i = colorSelect.options.length - 1; i >= 0; i--) {
+        if (colorSelect.options[i].selected) {
+            colorSelect.remove(i);
+        }
+    }
 });
-
